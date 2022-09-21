77b8f5c3ab3d48516bae76cdefbb47eb59379729
Federico Fracaro <federicohfracaro@gmail.com>
2022-09-21 16:18:54 -0300

    serverCorrecciones

--- serverFracaro.js
+++ serverFracaro.js
@@ -26,7 +26,7 @@ app.get('/productoRandom', async (req, res) => {
     })
 
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => {
         console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
         })
